c18333cdfaf944bc177a958ed0b9ad48ac345aed
Kiran <Kiran9573402192@gmail.com>
2022-09-22 11:44:55 +0530

    added code for few seleium methods

--- SelenumTrashCourse/src/main/java/NewTest.java
+++ SelenumTrashCourse/src/main/java/NewTest.java
@@ -1,6 +1,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,8 +19,9 @@ public class NewTest {
 	  driver.manage().deleteAllCookies();
 	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	  System.out.println("Hello I am here");
   }
-  @Test()
+  @Test(priority=1)
   public void keyin_Credentials() throws InterruptedException {
 	  WebElement Username_Entry=driver.findElement(By.name("username"));
 	  WebElement Password_Entry=driver.findElement(By.name("password"));
@@ -29,6 +31,27 @@ public class NewTest {
 	  Password_Entry.sendKeys("admin123");
 	  Thread.sleep(2000);
 	  Login_Button.click();
+	  Thread.sleep(4000);
+  }
+  @Test(priority=2)
+  public void fetchCSSProperties() {
+	  //to fetch al the css properties in a single go , we need to depend on the javascriptexecutor
+	  WebElement Login_Button=driver.findElement(By.xpath("//button[starts-with(@class,'oxd')]"));
+	  WebElement HeadeLogin=driver.findElement(By.xpath("//h5[text()='Login']"));
+	  System.out.println(HeadeLogin.getCssValue("font-size"));
+	  System.out.println(Login_Button.getCssValue("background-color"));
+	  System.out.println(Login_Button.getCssValue("font-size"));
+	  System.out.println(Login_Button.getCssValue("font-family"));
+	
+  }
+  @Test(priority=3)
+  public void checkKeyEnumClass() throws InterruptedException {
+	  WebElement Username_entry=driver.findElement(By.name("username"));
+	  Username_entry.sendKeys(Keys.chord(Keys.SHIFT,"admin"));
+	  Thread.sleep(2000);
+	  Username_entry.sendKeys(Keys.chord(Keys.CONTROL,"a"));
+	  Username_entry.sendKeys(Keys.chord(Keys.BACK_SPACE));
+	  //Username_entry.clear();
 	  Thread.sleep(4000);
   }
   @AfterTest()

cf4b5a11e8fa52a90f197ff8dc43036fabfeff04
Kiran <Kiran9573402192@gmail.com>
2022-09-24 21:47:36 +0530

    added code for few more methods

--- SelenumTrashCourse/src/main/java/NewTest.java
+++ SelenumTrashCourse/src/main/java/NewTest.java
@@ -18,10 +18,12 @@ public class NewTest {
 	  driver.manage().window().maximize();
 	  driver.manage().deleteAllCookies();
 	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-	  System.out.println("Hello I am here");
+	  //driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	  driver.get("https://www.google.co.in/");
+	  //driver.get("https://www.facebook.com/");
+	  //driver.get("https://demoqa.com/radio-button");
   }
-  @Test(priority=1)
+ /* @Test(priority=1)
   public void keyin_Credentials() throws InterruptedException {
 	  WebElement Username_Entry=driver.findElement(By.name("username"));
 	  WebElement Password_Entry=driver.findElement(By.name("password"));
@@ -54,6 +56,47 @@ public class NewTest {
 	  //Username_entry.clear();
 	  Thread.sleep(4000);
   }
+  @Test(priority=4)
+  public void verifySubmitfunction() throws InterruptedException {
+	  WebElement GoogleSearch_Input=driver.findElement(By.name("q"));
+	  GoogleSearch_Input.sendKeys("Facebook");
+	  WebElement Google_Submit=driver.findElement(By.name("btnK"));
+	  System.out.println(Google_Submit.getAttribute("data-ved"));
+	  //GetAttribute() method
+	  System.out.println(GoogleSearch_Input.getAttribute("name"));
+	  GoogleSearch_Input.submit();
+	  Thread.sleep(3000);
+  }*/
+ @Test(priority=5)
+ public void verifyWebelementFunctions() {
+	 /*WebElement Disabled_Button=driver.findElement(By.xpath("//input[@class='custom-control-input disabled']"));
+	 WebElement Yes_Button=driver.findElement(By.id("yesRadio"));
+	 WebElement GoogleSearch_Button=driver.findElement(By.name("btnK"));
+	 WebElement FacebookLogin_Button=driver.findElement(By.name("login"));
+	 System.out.println(FacebookLogin_Button.getAttribute("name"));
+	 System.out.println(FacebookLogin_Button.getLocation().getX()+"  "+FacebookLogin_Button.getLocation().getY());
+	 System.out.println(FacebookLogin_Button.getTagName());
+	 System.out.println(FacebookLogin_Button.getText());
+	 System.out.println(FacebookLogin_Button.getLocation());
+	 System.out.println(FacebookLogin_Button.getSize());
+	 System.out.println(FacebookLogin_Button.isDisplayed());
+	 System.out.println(FacebookLogin_Button.isEnabled());
+	 System.out.println(FacebookLogin_Button.isSelected());
+	 System.out.println(GoogleSearch_Button.getTagName());
+	 System.out.println(GoogleSearch_Button.getText());
+	 System.out.println(GoogleSearch_Button.getLocation());
+	 System.out.println(GoogleSearch_Button.getSize());
+	 System.out.println(GoogleSearch_Button.isDisplayed());
+	 System.out.println(GoogleSearch_Button.isEnabled());
+	 System.out.println(GoogleSearch_Button.isSelected());
+	 System.out.println(Disabled_Button.isEnabled());
+	 System.out.println(Disabled_Button.isDisplayed());
+	 System.out.println(Yes_Button.isDisplayed());*/
+	WebElement google=driver.findElement(By.xpath("//input[@name='q']"));
+	WebElement mikeIcon=driver.findElement(By.xpath("//div[@class='XDyW0e']"));
+	 System.out.println(google.isDisplayed());
+	 System.out.println(mikeIcon.isDisplayed());
+ }
   @AfterTest()
   public void tearDown() {
 	  driver.close();

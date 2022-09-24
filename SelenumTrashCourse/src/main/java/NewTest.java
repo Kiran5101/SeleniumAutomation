a1bac2ce383ee1104097559fe52fd14b8d37f0d7
Kiran <Kiran9573402192@gmail.com>
2022-09-24 21:51:18 +0530

    New code

--- SelenumTrashCourse/src/main/java/NewTest.java
+++ SelenumTrashCourse/src/main/java/NewTest.java
@@ -20,7 +20,7 @@ public class NewTest {
 	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	  //driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	  driver.get("https://www.google.co.in/");
-	  //driver.get("https://www.facebook.com/");
+	  driver.get("https://www.facebook.com/");
 	  //driver.get("https://demoqa.com/radio-button");
   }
  /* @Test(priority=1)
